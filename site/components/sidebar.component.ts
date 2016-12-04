032f7037068220b8a4231626ec5c3ca4d16a13ce
David Valero <dvdvalero89@gmail.com>
2016-12-04 12:40:55 +0100

    Added storage to sidebar

--- site/components/sidebar.component.ts
+++ site/components/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {TerrainService} from "../services/terrain.service";
 import {Minion} from "../objects/minion";
+import {ResourcesStorage} from "../objects/resource-storage";
 
 @Component({
     selector: 'sidebar',
@@ -11,19 +12,33 @@ import {Minion} from "../objects/minion";
                 <div class="minion"><div class="eye"></div></div>
             </div>
             <div class="indicators">
-                <div class="health" [style.width]="mn.getHealth()+'%'"></div>
-                <div class="energy" [style.width]="mn.getEnergy()+'%'"></div>
+                <div class="ind health" [style.width]="mn.getHealth()+'%'"></div>
+                <div class="ind energy" [style.width]="mn.getEnergy()+'%'"></div>
             </div>
         </div>
+        
+        <h1 style="margin-top: 25px">Recursos</h1>
+        <div class="minionAvatar" *ngFor="let st of storageList">
+            <div class="minionContainer">
+                <div class="storage"><i class="icon icon-box"></i></div>
+            </div>
+            <div class="indicators">
+                <div class="ind bold">Acumulado</div>
+                <div class="ind">{{st.getStored()}}</div>
+            </div>
+        </div>
+        
         
         <div class="button clear block" (click)="saveGameState()">Guardar <i class="icon-floppy"></i></div>
     `
 })
 export class SidebarComponent {
     minionList:Minion[];
+    storageList:ResourcesStorage[];
 
     constructor(private terrainService:TerrainService){
         this.minionList = terrainService.getMinionList();
+        this.storageList = this.terrainService.getStorageList();
     }
 
     saveGameState(){
